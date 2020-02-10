dcee7358f61364eeaec54e857cdae270a10ae359
WUR\weina005 <els.weinans@wur.nl>
2020-02-10 16:04:31 +0100

    fixed some issues

--- measurement_noise.m
+++ measurement_noise.m
@@ -1,22 +1,26 @@
 clear all
 rng('default')
+
+%% noisy data with jumps
 % load('dataset_a0_jumps_20sims_015_045')
 % load('dataset_a10_jumps_20sims_015_045')
 
 % load('dataset_4D_rp-05_jumps_20sims')
-% load('dataset_4D_rp22_jumps_20sims.mat') 
-% Lcs=Lras;
-% cs=ras;
+load('dataset_4D_rp22_jumps_20sims.mat') 
+Lcs=Lras;
+cs=ras;
 
+%% noisy data no jumps
 % load('dataset_a0_noise_20sims_015_045')
 % load('dataset_a10_noise_20sims_015_045')
 
 % load('dataset_4D_rp-05_noise_20sims')
-load('dataset_4D_rp22_noise_20sims')
+% load('dataset_4D_rp22_noise_20sims')
 
-Lcs=Lras;
-cs=ras;
+% Lcs=Lras;
+% cs=ras;
 
+%% analysis
 stepsnoise=100;
 data_old=data;
 noises=linspace(0,0.5,stepsnoise);
@@ -109,16 +113,16 @@ for i=1:12
 end
 
 
-figure
-for i=1:12    
-    subplot(6,2,i)
-    plot(cs,ind(i,:)','LineWidth',1.5)
-    title(indnames(i))
-    set ( gca, 'xdir', 'reverse' )
-    if i>10
-        xlabel('c')
-    end
-end
+% figure
+% for i=1:12    
+%     subplot(6,2,i)
+%     plot(cs,ind(i,:)','LineWidth',1.5)
+%     title(indnames(i))
+%     set ( gca, 'xdir', 'reverse' )
+%     if i>10
+%         xlabel('c')
+%     end
+% end
 
 figure
 bar(bars)
